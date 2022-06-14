84b6a4b726a3d118a4b497ddb1cdc7c206753ffa
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-06-15 09:58:23 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -6,4 +6,13 @@
     var api="wimr";
     //-------------------------------------------------------------------------------------
     m[p+"device-number"]     ={url:H+"/business/device-number/page.html",prefix:p,router:1};
+
+    m[p+"client"]    ={url:H+"/business/client/page.html",Table:"coreplus-client",form_module:'jsonv',prefix:p};
+    m[p+"referral"]  ={url:H+"/business/referral/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referrer"]  ={url:H+"/business/referrer/page.html",Table:"coreplus-referrer",form_module:'jsonv',prefix:p};
+    
+    m[p+"client-count"]         ={url:H+"/business/client-count/page.html",Table:"coreplus-client",form_module:'jsonv',prefix:p};
+    m[p+"client-count-chart"]   ={url:H+"/business/client-count-chart/page.html",Table:"coreplus-client",form_module:'jsonv',prefix:p};
+
+
 })();
