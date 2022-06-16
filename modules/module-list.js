3c87cb791388b6b7382e8d4014e163b8354780cb
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-06-16 13:53:30 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -13,7 +13,19 @@
     
     m[p+"client-count"]         ={url:H+"/business/client-count/page.html",Table:"coreplus-client",form_module:'jsonv',prefix:p};
     m[p+"client-count-chart"]   ={url:H+"/business/client-count-chart/page.html",Table:"coreplus-client",form_module:'jsonv',prefix:p};
-    m[p+"referral-count"]       ={url:H+"/business/referral-count/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
-    m[p+"referral-count-by-name-address"]       ={url:H+"/business/referral-count-by-name-address/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
-    m[p+"referral-count-by-name-address-2021"]       ={url:H+"/business/referral-count-by-name-address-2021/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name"]                   ={url:H+"/business/referral-count-by-name/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-chart"]             ={url:H+"/business/referral-count-by-name-chart/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-2021"]              ={url:H+"/business/referral-count-by-name-2021/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-2021-chart"]        ={url:H+"/business/referral-count-by-name-2021-chart/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-2022"]              ={url:H+"/business/referral-count-by-name-2022/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-2022-chart"]        ={url:H+"/business/referral-count-by-name-2022-chart/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-address"]           ={url:H+"/business/referral-count-by-name-address/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-address-2021"]      ={url:H+"/business/referral-count-by-name-address-2021/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-name-address-2022"]      ={url:H+"/business/referral-count-by-name-address-2022/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-year"]                   ={url:H+"/business/referral-count-by-year/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-year-chart"]             ={url:H+"/business/referral-count-by-year-chart/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-month-2022"]              ={url:H+"/business/referral-count-by-month-2022/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-month-2022-chart"]        ={url:H+"/business/referral-count-by-month-2022-chart/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-month-2021"]              ={url:H+"/business/referral-count-by-month-2021/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
+    m[p+"referral-count-by-month-2021-chart"]        ={url:H+"/business/referral-count-by-month-2021-chart/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
 })();
