35bacba04bf42773f46104e9861d09fd3b158de7
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-07-19 16:06:36 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -52,12 +52,28 @@
     m[p+"marketing-campaign-gp-list-2022-04"]              ={url:H+"/business/marketing-campaign/gp-list-2022-04/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
     m[p+"marketing-campaign-referral-count-2022-4-5-6"]    ={url:H+"/business/marketing-campaign/referral-count-2022-4-5-6/page.html",Table:"coreplus-referral",form_module:'jsonv',prefix:p};
     
-    m[p+"revenue-all-year-data"]          ={url:H+"/business/revenue/all-year-data/page.html"};
-    m[p+"revenue-all-year-chart"]         ={url:H+"/business/revenue/all-year-chart/page.html"};
-    m[p+"revenue-2021-data"]              ={url:H+"/business/revenue/2021-data/page.html"};
-    m[p+"revenue-2021-chart"]             ={url:H+"/business/revenue/2021-chart/page.html"};
-    m[p+"revenue-2022-data"]              ={url:H+"/business/revenue/2022-data/page.html"};
-    m[p+"revenue-2022-chart"]             ={url:H+"/business/revenue/2022-chart/page.html"};
-    m[p+"revenue-all-year-by-site-data"]          ={url:H+"/business/revenue/all-year-by-site-data/page.html"};
-    m[p+"revenue-all-year-by-site-chart"]         ={url:H+"/business/revenue/all-year-by-site-chart/page.html"};
+    m[p+"revenue-all-year-data"]          ={url:H+"/business/revenue/all/all-year-data/page.html"};
+    m[p+"revenue-all-year-chart"]         ={url:H+"/business/revenue/all/all-year-chart/page.html"};
+    m[p+"revenue-2021-data"]              ={url:H+"/business/revenue/all/2021-data/page.html"};
+    m[p+"revenue-2021-chart"]             ={url:H+"/business/revenue/all/2021-chart/page.html"};
+    m[p+"revenue-2022-data"]              ={url:H+"/business/revenue/all/2022-data/page.html"};
+    m[p+"revenue-2022-chart"]             ={url:H+"/business/revenue/all/2022-chart/page.html"};
+    
+    m[p+"revenue-all-year-by-site-data"]      ={url:H+"/business/revenue/site/all-year-data/page.html"};
+    m[p+"revenue-all-year-by-site-chart"]     ={url:H+"/business/revenue/site/all-year-chart/page.html"};
+    m[p+"revenue-2022-by-site-data"]          ={url:H+"/business/revenue/site/2022-data/page.html"};
+    m[p+"revenue-2022-by-site-chart"]         ={url:H+"/business/revenue/site/2022-chart/page.html"};
+    m[p+"revenue-2021-by-site-data"]          ={url:H+"/business/revenue/site/2021-data/page.html"};
+    m[p+"revenue-2021-by-site-chart"]         ={url:H+"/business/revenue/site/2021-chart/page.html"};
+    
+    m[p+"revenue-2022-by-doctor-data"]          ={url:H+"/business/revenue/doctor/2022-data/page.html"};
+    m[p+"revenue-2022-by-doctor-chart"]         ={url:H+"/business/revenue/doctor/2022-chart/page.html"};
+    m[p+"revenue-2021-by-doctor-data"]          ={url:H+"/business/revenue/doctor/2021-data/page.html"};
+    m[p+"revenue-2021-by-doctor-chart"]         ={url:H+"/business/revenue/doctor/2021-chart/page.html"};
+
+    m[p+"patient-count-by-doctor-data"]          ={url:H+"/business/patient/count-by-doctor-data/page.html"};
+    m[p+"patient-count-by-doctor-chart"]         ={url:H+"/business/patient/count-by-doctor-chart/page.html"};
+    
+    m[p+"referral-all-year-data"]          ={url:H+"/business/referral/all-year-data/page.html"};
+
 })();
